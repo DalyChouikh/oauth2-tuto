94f0847e6dd29b37247224981adf1028c3ebc2d6
DalyChouikh <chouikhdaly215@gmail.com>
2023-02-21 20:38:17 +0100

    Initial commit

---
+++ src/main/java/dev/daly/oauth2tuto/Oauth2TutoApplication.java
@@ -0,0 +1,13 @@
+package dev.daly.oauth2tuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2TutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2TutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/daly/oauth2tuto/Oauth2TutoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daly.oauth2tuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2TutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
